0b379313db9421a9e96d5495d3d9fdaace28371d
srivatsan <srivatsan25297@gmail.com>
2023-12-21 21:43:35 -0500

    fixed button click issue and changed vision prompt to only identify groceries

--- models/visionmodels.py
+++ models/visionmodels.py
@@ -1,6 +1,6 @@
 from . import vision
 
 def get_vanilla_model(name = "vanilla-vision"):
-    return vision.GeminiVisionModel(name, "Identify the items in the fridge and return it as a list")
+    return vision.GeminiVisionModel(name, "Identify the groceries in the fridge and return it as a list. If you could not find any groceries return an empty list ")
 
 get_default_model = get_vanilla_model
